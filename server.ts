43892d186345d809a7c8e9feccceafa28e5b3457
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-06 13:51:48 +0530

    setup express

--- server.ts
+++ server.ts
@@ -1,2 +1,12 @@
-console.log("Hello world")
+import app from "./src/app";
 
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
+  
+};
+
+startServer();

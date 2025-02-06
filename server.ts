8b1d14c6c2498fdbd48c71b01eb948cc2e3839e6
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-06 14:13:40 +0530

    config

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
   });
-  
+
 };
 
 startServer();

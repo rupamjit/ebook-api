b6a4015fb5aee5cfaf17d64daf791d079fce4b1a
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-07 15:07:32 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
-
 import express from "express";
 import globalErrorHandler from "./middleware/globalErrorHandler";
-
+import cors from "cors";
 import userRouter from "./user/userRouter";
 import bookRoute from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
-app.use(express.json())
+
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
+
+app.use(express.json());
 // Routes
 // app.get("/", (req, res) => {
 //   res.json({ message: "ebook api" });
 // });
 
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/books",bookRoute)
-
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/books", bookRoute);
 
 // Global error handler
 
-
 app.use(globalErrorHandler);
-
 
 export default app;
